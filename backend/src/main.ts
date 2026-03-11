b88a87b586e086496664de4bae50879a6b648812
TonNom <tonemail@example.com>
2026-03-11 20:35:10 +0000

    port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:1101',
+    origin: [
+      'http://localhost:1101',        // dev local
+      'http://168.231.82.55',         // front servi en HTTP sur VPS
+      'http://168.231.82.55:1101',    // si tu sers le front sur ce port en VPS
+    ],
     credentials: true,
   });
   await app.listen(process.env.PORT ?? 1102);
