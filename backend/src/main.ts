914f9b4ff2330929027dbeb6e9ddec4f97416abd
TonNom <tonemail@example.com>
2026-03-11 20:16:49 +0000

    port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4000',
+    origin: 'http://localhost:1101',
     credentials: true,
   });
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 1102);
 }
 bootstrap();
