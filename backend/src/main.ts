ecfbb917b0ec4ce393b57db8035e8e86f49cb447
TonNom <tonemail@example.com>
2026-03-11 20:52:54 +0000

    deactive cros

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,13 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // CORS très permissif : accepte toutes les origines (pratique pour dev local + VPS)
   app.enableCors({
-    origin: [
-      'http://localhost:1101',        // dev local
-      'http://168.231.82.55',         // front servi en HTTP sur VPS
-      'http://168.231.82.55:1101',    // si tu sers le front sur ce port en VPS
-    ],
+    origin: true,          // reflète automatiquement l'origine de la requête
     credentials: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
   });
   await app.listen(process.env.PORT ?? 1102);
 }
